759407659103c64782a4b12eeca00f9b2b0051a0
Artur <kartartur@mail.ru>
2025-04-02 16:15:49 +0300

    Added RegistrationSevice for user registration

---
+++ UserDBApp/Services/RegistrationService.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+
+class RegistrationService: ObservableObject {
+    private var userService: UserServiсe
+
+    init(userService: UserServiсe) {
+        self.userService = userService
+    }
+
+    func register(request: RegistrationRequest) async -> Result<Void, Error> {
+        do {
+            let tokenResponse = try await userService.token(from: UserServiсe.Endpoint.users)
+            let token = tokenResponse.token
+
+            let response = try await userService.newUser(
+                from: UserServiсe.Endpoint.users.rawValue,
+                user: request.user,
+                token: token
+            )
+
+            if response.success {
+                return .success(())
+            } else {
+                return .failure(NSError(domain: "RegistrationError", code: 0, userInfo: [NSLocalizedDescriptionKey: response.message]))
+            }
+        } catch {
+            return .failure(error)
+        }
+    }
+}
