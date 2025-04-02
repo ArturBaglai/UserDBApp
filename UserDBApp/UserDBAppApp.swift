7b696f286aa85af18313a30727924ad02de93850
Artur <kartartur@mail.ru>
2025-04-02 15:37:12 +0300

    Initial Commit

---
+++ UserDBApp/UserDBAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDBAppApp.swift
+//  UserDBApp
+//
+//  Created by Artur on 02.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct UserDBAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
