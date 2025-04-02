abe888bd89c37137b1341df837b62a7866de1272
Artur Baglai <40723380+ArturBaglai@users.noreply.github.com>
2025-04-02 16:02:41 +0300

    Update UserDBAppApp.swift
    Deleted comments and ContentView

--- UserDBApp/UserDBAppApp.swift
+++ UserDBApp/UserDBAppApp.swift
@@ -1,17 +1,10 @@
-//
-//  UserDBAppApp.swift
-//  UserDBApp
-//
-//  Created by Artur on 02.04.2025.
-//
-
 import SwiftUI
 
 @main
 struct UserDBAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            
         }
     }
 }
