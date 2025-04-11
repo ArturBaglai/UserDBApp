99428e4a6206dda03faa6e7880d83ac4ba81f23c
Artur <kartartur@mail.ru>
2025-04-11 14:16:54 +0300

    Removed import combine

---
+++ UserDBApp/Networking/NetworkMonitor.swift
@@ -0,0 +1,24 @@
+import Network
+import Combine
+
+@MainActor
+class NetworkMonitor: ObservableObject {
+    static let shared = NetworkMonitor()
+
+    private let monitor = NWPathMonitor()
+    private let queue = DispatchQueue(label: "NetworkMonitorQueue")
+
+    @Published var isConnected: Bool = false
+
+    private init() {
+        monitor.pathUpdateHandler = { [weak self] path in
+            DispatchQueue.main.async {
+                let newStatus = path.status == .satisfied
+                if self?.isConnected != newStatus {
+                    self?.isConnected = newStatus
+                }
+            }
+        }
+        monitor.start(queue: queue)
+    }
+}
