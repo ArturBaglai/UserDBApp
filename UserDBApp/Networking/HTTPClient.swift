0972023506c9a4d9cca4ee35efc1642d0548e97a
Artur <kartartur@mail.ru>
2025-04-11 14:17:43 +0300

    Removed  unuseful cases

--- UserDBApp/Networking/HTTPClient.swift
+++ UserDBApp/Networking/HTTPClient.swift
@@ -1,16 +1,9 @@
 import Foundation
-import OSLog
 
 extension HTTPClient {
     enum Method: String {
         case get = "GET"
-        case put = "PUT"
         case post = "POST"
-        case delete = "DELETE"
-        case head = "HEAD"
-        case options = "OPTIONS"
-        case trace = "TRACE"
-        case connect = "CONNECT"
     }
 }
 
