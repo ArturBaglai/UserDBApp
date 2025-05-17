48dd583cce6e8262a8f49781342e10cea400303a
Artur <kartartur@mail.ru>
2025-05-17 22:10:17 +0300

    Modified if photo.isEmpty {             validationErrors["photo"] = ["Photo is required"]         } and added function clearErrorIfFieldIsEmpty

--- UserDBApp/Models/UserInputViewModel.swift
+++ UserDBApp/Models/UserInputViewModel.swift
@@ -4,6 +4,7 @@ class UserInputViewModel: ObservableObject {
     @Published var name: String = ""
     @Published var email: String = ""
     @Published var phone: String = ""
+    @Published var photo: Data = Data()
     @Published var selectedOption: String = ""
     @Published var positionId: Int = 0
     @Published var validationErrors: [String: [String]] = [:]
@@ -12,18 +13,26 @@ class UserInputViewModel: ObservableObject {
         validationErrors = [:]
         
         if name.isEmpty {
-            validationErrors["name"] = ["Name is required"]
+            validationErrors["name"] = ["Required field"]
         }
         if email.isEmpty {
             validationErrors["email"] = ["Invalid email adress"]
         }
         if phone.isEmpty {
-            validationErrors["phone"] = ["Phone number is required"]
+            validationErrors["phone"] = ["Required field"]
+        }
+        if photo.isEmpty {
+            validationErrors["photo"] = ["Photo is required"]
         }
         return validationErrors.isEmpty
     }
     private func isValidEmail(_ email: String) -> Bool {
         let emailRegex = "^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"
-        return NSPredicate(format: "SELf MATCHES %@", emailRegex).evaluate(with: email)
+        return NSPredicate(format: "SELF MATCHES %@", emailRegex).evaluate(with: email)
+    }
+    func clearErrorIfFieldIsEmpty(field: String, value: String) {
+        if value.trimmingCharacters(in: .whitespaces).isEmpty {
+            validationErrors.removeValue(forKey: field)
+        }
     }
 }
