4f65413299f8028c0c2f935f611470f0d88c6f0f
Artur <kartartur@mail.ru>
2025-05-17 21:40:04 +0300

    Created camera prewview

---
+++ UserDBApp/Modules/Main/CameraPreview.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+import AVFoundation
+
+struct CameraPreview: UIViewRepresentable {
+    
+    @ObservedObject var viewModel: CameraViewModel
+    
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView(frame: UIScreen.main.bounds)
+        let previewLayer = viewModel.getPreviewLayer()
+        previewLayer.frame = view.bounds
+        view.layer.addSublayer(previewLayer)
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        DispatchQueue.main.async {
+            if let previewLayer = uiView.layer.sublayers?.first as? AVCaptureVideoPreviewLayer {
+                previewLayer.frame = uiView.bounds
+            }
+        }
+    }
+}
