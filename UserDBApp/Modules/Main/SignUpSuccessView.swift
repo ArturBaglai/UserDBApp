4b06d4e0d2992fbdff3d877e316048384c4de4be
Artur <kartartur@mail.ru>
2025-05-17 21:50:25 +0300

    Created SignUpSuccessView

---
+++ UserDBApp/Modules/Main/SignUpSuccessView.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+
+public struct SignUpSuccessView: View {
+    
+    public var body: some View {
+        NavigationView {
+            VStack {
+                Image("SignUpSuccess")
+                Text ("User successfully registered")
+                NavigationLink(destination: MainView())
+                {
+                    Text("Got it")
+                        .frame(maxWidth: 140)
+                        .padding()
+                        .background(.yellow)
+                        .foregroundColor(.black)
+                        .cornerRadius(100)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    SignUpSuccessView()
+}
