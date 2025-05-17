0183c11c5a2881a28cf8959c6766ee692389789e
Artur <kartartur@mail.ru>
2025-05-17 21:30:40 +0300

    Modified RegistrationService

--- UserDBApp/Services/RegistrationService.swift
+++ UserDBApp/Services/RegistrationService.swift
@@ -9,12 +9,13 @@ class RegistrationService: ObservableObject {
 
     func register(request: RegistrationRequest) async -> Result<Void, Error> {
         do {
-            let tokenResponse = try await userService.token(from: UserServiсe.Endpoint.users)
+            let tokenResponse = try await userService.token(from: UserServiсe.Endpoint.token)
             let token = tokenResponse.token
 
             let response = try await userService.newUser(
                 from: UserServiсe.Endpoint.users.rawValue,
                 user: request.user,
+                photoData: request.photo,
                 token: token
             )
 
